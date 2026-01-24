965ebbc7407e84e52dd971cafd6bf13ac819b104
abdalakhras <abdakalakhras@gmail.com>
2026-01-25 02:31:46 +0300

    adding cart and qntty

--- cart.js
+++ cart.js
@@ -5,14 +5,42 @@ function Cart(){
 var myList = JSON.parse(data)
 console.log(typeof(myList),myList)
 
-const data2 = localStorage.getItem('productId')
-var productId = Number(data2)
-console.log(typeof(productId),productId)
+const data3 = localStorage.getItem('cartList')
+var cartList = JSON.parse(data3)
+
+// const data2 = localStorage.getItem('productId')
+// var productId = Number(data2)
+// console.log(typeof(productId),productId)
 
 
-    const filterd = myList.filter((itm)=>{
-        return (itm.id == productId)
-    })
-    console.log('filterd Obj',filterd)
+    // const filterd = myList.filter((itm)=>{
+    //     return (itm.id == productId)
+    // })
+    // console.log('filterd Obj',filterd)
+
+    var row = document.getElementById('row')
+
+    cartList.forEach(card => {
+           
+    const col = document.createElement('div')
+    col.classList.add('col')
+
+    col.innerHTML = `
+    
+    <div class="card" style="width: 18rem;">
+  <img src="${card.imgUrl}" class="card-img-top" alt="img cannot be shown">
+  <div class="card-body">
+    <h5 class="card-title">${card.name}</h5>
+    <p class="card-text">${card.discription}</p>
+    <p>${card.price} $</p>
+           <p># of products ${card.qnt}</p>
+    <a href="" class="btn btn-primary">Add to cart</a>
+  </div>
+</div>
+
+    `
+    row.appendChild(col)
+    });
+  
 }
 Cart()
--- task.js
+++ task.js
@@ -26,6 +26,55 @@ var myList = [
     imgUrl:'https://i5.walmartimages.com/seo/SAMSUNG-32-Class-FHD-1080P-Smart-LED-TV-UN32N5300_2b2943fd-73d6-4d7b-9c54-e22db0c660f1_4.e79d68ec3a718064170de6cbd82e6030.jpeg'}
 ]
 
+function ViewList(){
+  var row = document.getElementById('row')
+
+  myList.forEach((obj)=>{
+  
+  const col = document.createElement('div')
+    col.classList.add('col')
+    col.innerHTML = `
+      
+       <div class="card" style="width: 18rem;">
+        <img src="${obj.imgUrl}" class="card-img-top" alt="img cannot be shown">
+        <div class="card-body">
+        <h5 class="card-title">${obj.name}</h5>
+        <p class="card-text">${obj.discription}</p>
+        <p>${obj.price} $</p>
+        <button class="btn btn-primary add-to-cart" data-id="${obj.id}">add to cart</button>
+        </div>
+        </div>
+
+        `
+         row.appendChild (col)
+
+    const btn = col.querySelector('.add-to-cart')
+    btn.addEventListener('click',(e)=>{
+      let existObj = cartList.find(ob=> ob.id == obj.id)
+      if(existObj){
+        existObj.qnt +=1
+      }else{
+         cartList.push({...obj,qnt:1})
+      }
+     
+      localStorage.setItem('cartList',JSON.stringify(cartList))
+  console.log('obj od this specifc col', obj)
+  console.log('cartList :',cartList)
+
+    })
+  })
+}
+
+ViewList()
+
+
+
+
+
+
+
+
+var cartList = []
 var myform = document.getElementById('myform')
 
 myform.addEventListener('submit',(e)=>{
@@ -75,9 +124,22 @@ console.log(myList)
 var btn = col.querySelector('.add-to-cart')
 btn.addEventListener('click',(e)=>{
 
- let id = e.target.dataset.id
- console.log('id from button click',id)
-  localStorage.setItem('productId',(e.currentTarget.dataset.id))
+//  let id = e.target.dataset.id
+//  console.log('id from button click',id)
+//   localStorage.setItem('productId',(e.currentTarget.dataset.id))
+  console.log('id of this button',itm.id)
+
+  let existProduct = cartList.find(prod => prod.id == itm.id)
+
+  if(existProduct){
+    existProduct.qnt +=1
+  }else{
+    cartList.push({...itm,qnt:1})
+  }
+  
+  localStorage.setItem('cartList',JSON.stringify(cartList))
+  console.log('obj od this specifc col', itm)
+   console.log('cartList :',cartList)
 })
 
        
